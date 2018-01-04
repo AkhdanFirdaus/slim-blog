c20ecf4bb150fd976d0aaa1ab0c79bfc6079b11a
Akhdan <akhdan.musyaffa.firdaus@gmail.com>
2018-01-04 20:46:23 +0700

    Post model v1.0

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $dates = ['created_at'];
+
+    public function setTitleAttribute($value)
+    {
+    	$this->attributes['title'] = $value;
+
+    	if (! $this->exist)
+    	{
+    		$this->attributes['slug'] = str_slug($value);
+    	}
+    }
 }
